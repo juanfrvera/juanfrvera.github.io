316521a66f018d1bf207ba8fdbddc36806bb5299
Juan Vera <juanfrvera@gmail.com>
2023-04-18 22:00:39 -0300

    wip: experience

---
+++ src/components/experience.svelte
@@ -0,0 +1,10 @@
+<h3 id="title">
+	<slot name="company" />
+</h3>
+<slot name="roles" />
+
+<style>
+	#title {
+		margin: 0;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-This is a test
+<script>
+	import Experience from '../components/experience.svelte';
+</script>
+
+I am FullStack a web developer with 3+ years of professional experience
+
+<h2>Experience</h2>
+
+<Experience>
+	<span slot="company">DevSavant</span>
+	<span slot="roles">FullStack developer</span>
+</Experience>
+<Experience>
+	<span slot="company">Blaise</span>
+	<span slot="roles">FullStack developer | Project Manager</span>
+</Experience>
+<Experience>
+	<span slot="company">ITsynch</span>
+	<span slot="roles">Software Engineer</span>
+</Experience>
