6bd55db30795df1daa9b4a6dc3347e320cb815dc
juanfrvera <juanfrvera.work@gmail.com>
2023-04-21 15:41:30 -0300

    Updates

---
+++ _app/immutable/chunks/1.1c931c02.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.d7e6cf4f.js";export{t as component};
--- _app/immutable/chunks/1.cdff7135.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.6ed0969d.js";export{t as component};
