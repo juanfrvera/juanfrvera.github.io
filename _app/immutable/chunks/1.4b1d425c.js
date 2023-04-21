af9b1ff2683fa5de125e8c4e06e79d90f07e1ac5
Juan Vera <juanfrvera@gmail.com>
2023-04-21 09:19:56 -0300

    Updates

--- _app/immutable/chunks/1.4b1d425c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.9b78f2e3.js";export{t as component};
---
+++ _app/immutable/chunks/1.cdff7135.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.6ed0969d.js";export{t as component};
